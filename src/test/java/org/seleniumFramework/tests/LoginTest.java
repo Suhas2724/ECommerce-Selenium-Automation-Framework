16443869e48d788d010491c3946bcf2d9c7b863a
suhas.channavar <suhas.channavar@thoughtworks.com>
2025-05-24 22:13:14 +0530

    code formating

--- src/test/java/org/seleniumFramework/tests/LoginTest.java
+++ src/test/java/org/seleniumFramework/tests/LoginTest.java
@@ -1,27 +1,15 @@
 package org.seleniumFramework.tests;
 
-import org.apache.logging.log4j.Logger;
 import org.seleniumFramework.listeners.RetryAnalyzer;
 import org.seleniumFramework.listeners.TestListener;
-import org.seleniumFramework.pages.HomePage;
 import org.seleniumFramework.pojo.User;
-import org.seleniumFramework.utils.LoggerUtility;
-import org.slf4j.LoggerFactory;
-import org.testng.annotations.AfterMethod;
-import org.testng.annotations.BeforeMethod;
 import org.testng.annotations.Listeners;
 import org.testng.annotations.Test;
 
-import static org.seleniumFramework.constants.Browser.CHROME;
 import static org.testng.Assert.assertEquals;
 
 @Listeners(TestListener.class)
 public class LoginTest extends BaseTest{
-
-
-
-
-
 
     @Test(description = "Login test", groups = {"e2e", "sanity"}, dataProviderClass = org.seleniumFramework.dataproviders.LoginDataProvider.class, dataProvider = "LoginTestDataProvider", retryAnalyzer = RetryAnalyzer.class)
     public void loginTest(User user) {
